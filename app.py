b1f7b44611097810bbe829f084f29b9b945ff270
Zebulun Arendsee <arendsee@iastate.edu>
2015-12-25 02:10:14 -0600

    Wrap top level ttk objects in classes
    I am testing an overall OOP organization for the GUI. The top level is
    the MyApp class, which initializes the root. Children of the root are
    all classes inheriting from ttk classes. For example MyButton inherits
    from Button. This allows the overall norms of the program to be uniform
    and simplifies the organization (and readability) of the main class. It
    allows the main MyApp class to only handle the higher level organization
    of the app, without the clutter of lowerlevel details. I have made My*
    classes for Button, Frame and Notebook, so far.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from tkinter import *
 from tkinter import ttk
 
-class ZButton(Button):
+class MyButton(Button):
     def __init__(self, title, column, row, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configure(text = title, command = self.clicked)
@@ -12,14 +12,14 @@ class ZButton(Button):
     def clicked(self):
         raise NotImplemented 
 
-class MyButton(ZButton):
+class MyButton(MyButton):
     def clicked(self):
         if(self['text'] == 'click me'): 
             self.configure(text='hahaha')
         else:
             self.configure(text='click me')
 
-class PrintButton(ZButton):
+class PrintButton(MyButton):
     def __init__(self, text, *args, **kwargs):
         self.text = text
         super().__init__(*args, **kwargs)
@@ -27,7 +27,7 @@ class PrintButton(ZButton):
     def clicked(self):
         print(self.text)
 
-class ExitButton(ZButton):
+class ExitButton(MyButton):
     def __init__(self, root, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.root = root 
@@ -36,26 +36,36 @@ class ExitButton(ZButton):
     def clicked(self):
         self.root.destroy()
 
+class MyNotebook(ttk.Notebook):
+    def __init__(self, root, *args, **kwargs):
+        super().__init__(master=root, *args, **kwargs)
+        self.grid(column=0, row=0, sticky=(N, W, E, S))
+
+class MyFrame(ttk.Frame):
+    def __init__(self, root, title,  *args, **kwargs):
+        super().__init__(master=root, *args, **kwargs)
+        self.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.title=title
+
+    def add_to_notebook(self, nb):
+        nb.add(self, text=self.title)
+
+
 class MyApp:
     def __init__(self):
         root = Tk()
         root.title('Test of an app')
-        self.nb = ttk.Notebook(root)
-        self.nb.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.nb = MyNotebook(root)
 
-        self.frame_1 = ttk.Frame(self.nb)
-        self.frame_1.grid(column=0, row=0, sticky=(N, W, E, S))
-        self.button1 = MyButton(master=self.frame_1, title='click me', row=0, column=0)
-        self.button_exit = ExitButton(root, master=self.frame_1, title='exit', row=0, column=1)
-        self.nb.add(self.frame_1, text='Frame One')
-
-
+        self.f1 = MyFrame(root, title="Frame One")
+        self.f1.add_to_notebook(self.nb)
+        self.button1 = MyButton(master=self.f1, title='click me', row=0, column=0)
+        self.button_exit = ExitButton(root, master=self.f1, title='exit', row=0, column=1)
         
-        self.frame_2 = ttk.Frame(self.nb)
-        self.frame_2.grid(column=0, row=0, sticky=(N, W, E, S))
-        self.pbut1 = PrintButton(text='1', master=self.frame_2, title='print 1', row=1, column=0)
-        self.pbut2 = PrintButton(text='2', master=self.frame_2, title='print 2', row=1, column=2)
-        self.nb.add(self.frame_2, text='Frame Two')
+        self.f2 = MyFrame(root, title="Frame Two")
+        self.f2.add_to_notebook(self.nb)
+        self.pbut1 = PrintButton(text='1', master=self.f2, title='print 1', row=1, column=0)
+        self.pbut2 = PrintButton(text='2', master=self.f2, title='print 2', row=1, column=2)
 
         root.mainloop()
 
