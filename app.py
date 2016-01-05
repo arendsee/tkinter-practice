39f46edcb369323b5bd15c249b08f44847140161
Zebulun Arendsee <arendsee@iastate.edu>
2016-01-05 12:35:02 -0600

    Add Labelframe nested in Panedwindow in Notebook
    There are some seemingly weird issues with width and height.
    
    I have three Labelframe objects (nf1, fn2, nf3), in a vertically
    oriented Panedwindow. When I don't set the height for any window, nf3
    completely fills the available space. Basically, unless a height is set,
    the last fills everything. If a height is set, it is treated as the
    minimum.

--- app.py
+++ app.py
@@ -117,10 +117,12 @@ class MyApp:
         # --- Nested frame
         self.f2 = MyPanedwindow(root, title='Frame Two', orient=VERTICAL)
         self.f2.add_to_notebook(self.nb)
-        nf1 = ttk.Labelframe(self.f2, text="Nested Frame 1", width=100, height=100)
-        nf2 = ttk.Labelframe(self.f2, text="Nested Frame 2", width=100, height=100)
+        nf1 = ttk.Labelframe(self.f2, text="Nested Frame 1", height=30, width=100)
+        nf2 = ttk.Labelframe(self.f2, text="Nested Frame 2", width=100)
+        nf3 = ttk.Labelframe(self.f2, text="Nested Frame 3", width=100)
         self.f2.add(nf1)
         self.f2.add(nf2)
+        self.f2.add(nf3)
 
         # ----------------------
         # --- MessageBox buttons
